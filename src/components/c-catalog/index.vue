80779a9aee27fc317c2528981eaed54d8cfeaf48
cynthyxiang(向莹鑫) <cynthyxiang@tencent.com>
2019-05-30 20:55:11 +0800

    20190530

--- src/components/c-catalog/index.vue
+++ src/components/c-catalog/index.vue
@@ -1,15 +1,19 @@
 <template>
     <section class="timeline">
-        <div class="timeline__node">
-            <img src=""/>
+        <div class="timeline__node" v-for="item in dataList" :key="item"> 
+            <img :src="item.img"/>
             <div class="timeline__node__content">
-                <h1 style="margin:0">大型项目前端架构浅谈</h1>
-                <p class="timeline__node__content__desc">我在2年之前，写过一篇中小型项目的前端架构浅谈。随着能力的上升，以及在阿里巴巴工作的经验，是时候写一篇大型项目的前端架构分析了。 本篇文章不会更多侧重于具体技术实现，而是尝...</p>
+                <h1 style="margin:0">{{item.title}}</h1>
+                <p class="timeline__node__content__desc">{{item.desc}}</p>
                 <div class="timeline__node__actionbar">
-                    <span class="timeline__node__action">点赞 </span>
-                    <span class="timeline__node__action"> 评论</span>
+                    <span class="timeline__node__action">{{item.publishTime}}</span>
+                    <span class="timeline__node__action">点赞({{item.like}})</span>
+                    <span class="timeline__node__action"> 评论({{item.comment}})</span>
                     <span class="timeline__node__action"> 分享</span>
                 </div>
+            </div>
+            <div class="timeline__node__tail">
+                
             </div>
         </div>
     </section>
@@ -20,7 +24,18 @@
 export default {
   data() {
     return {
-
+        total : 0,
+        next : 0,
+        num : 0,
+        dataList:[{
+                "title" : "大型项目前端架构浅谈",
+                "desc" : "我在2年之前，写过一篇中小型项目的前端架构浅谈。随着能力的上升，以及在阿里巴巴工作的经验，是时候写一篇大型项目的前端架构分析了。 本篇文章不会更多侧重于具体技术实现，而是尝...",
+                "img" : "src/assets/images/pic01.jpg",
+                "publishTime" : "2018-12-20 00:00:00",
+                "tags" : ["必知必会","前端宝典"],
+                "comment" : 5,
+                "like" : 30
+            }]
     }
   },
   props:{
@@ -35,7 +50,7 @@ export default {
 <style scoped lang="scss">
     .timeline{
         position:relative;
-        padding:20px 20% 20px 10%;
+        padding:20px 10%;
         &__node{
             display: flex;
             position: relative;
@@ -46,8 +61,8 @@ export default {
             text-align: left;
             & img{
                 display:block;
-                max-width: 200px;
-                max-height: 200px;
+                width: 200px;
+                height: 200px;
                 margin-right: 20px;
             }
             &__content{
@@ -56,15 +71,35 @@ export default {
                 &__desc{
                     flex:1;
                 }
-
             }
             &__action{
                 font-size:12px;
                 float:right;
                 padding:4px 10px;
+                line-height:1;
                 cursor:pointer;
                 &:hover{
                     color:#3366ff;
+                }
+            }
+            &__time{
+                font-size:12px;
+                position:absolute;
+                top: calc(50% - 1.5em);
+                right:-6em;
+                padding: 1em;
+                // background: white;
+                // border-radius: 0% 100% 100% 0%/ 100% 100% 100% 100%;
+                z-index: 2;
+                &::before{
+                    position:absolute;
+                    top: calc(50% - 1.5em);
+                    right:-6em;
+                    width: 1em;
+                    height: 1em;
+                    background: white;
+
+
                 }
             }
 
