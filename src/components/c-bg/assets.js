ef339d59f55ba9dc74ff08c59e6bb3d18a7f65c9
cynthyxiang(向莹鑫) <cynthyxiang@tencent.com>
2019-07-09 14:38:09 +0800

    [+]dist

--- src/components/c-bg/assets.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-console.log(path)
-const base = '/src/assets/images/gallery/fulls/'
-let bgImage = [
-]
-for (let i = 1; i < 10; i++) {
-  bgImage.push(`url(${base}0${i}.jpg)`)
-}
-export default bgImage
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import router from './router'
-import App from './App.vue'
-
-// import 'assets/font/iconfont'
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
